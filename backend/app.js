e68909b50793e13fb309df48cbf4669e0e14aa12
andavarapulikith <likithandavarapuiiits@gmail.com>
2023-11-20 18:55:28 +0530

    added contact us routes

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const articlesRoutes = require('./routes/articlesRoutes');
 const userRoutes = require('./routes/userRoutes');
-
+const utilityRoutes=require('./routes/utilityRoutes')
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
@@ -13,7 +13,7 @@ app.use(cors());
 app.use('/auth',authRoutes);
 app.use('/articles',articlesRoutes);
 app.use('/user',userRoutes)
-
+app.use('/utility',utilityRoutes)
 
 
 module.exports = app
