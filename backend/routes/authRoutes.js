ea894b1be0559ff760b5d7df731dc50c5b1956b3
andavarapulikith <likithandavarapuiiits@gmail.com>
2023-12-03 15:04:42 +0530

    added forgot password implementation

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,5 +7,6 @@ router.post('/register',authController.register_post);
 router.get('/checkEmail/:email',authController.checkEmail_get);
 router.delete('/:expertid',authController.remove_Expert)
 router.put('/:expertid/updateblocked',authController.updateblockedstate)
-
+router.post('/forgotpassword',authController.forgotpassword)
+router.post('/passwordchange',authController.changepassword)
 module.exports = router;
