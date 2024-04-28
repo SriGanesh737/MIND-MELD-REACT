0413945870486fd2434fe48d05a697c8b6e4a04f
Sri Ganesh <sriganeshiiits@gmail.com>
2024-04-28 12:21:02 +0530

    Implemented Docker

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,17 @@ const mongoose = require("mongoose");
 
 dotenv.config();
 const port = process.env.PORT || 8000;
-const myUri = process.env.myUri;
-const connectionParams = { useNewUrlParser: true };
+const mongoUri = process.env.myUri;
 
-mongoose.connect(myUri, connectionParams).then(() => {
-  app.listen(port, () => {
-    console.log("connected to database & app listening on port " + port);
+const connectionParams = {
+  useNewUrlParser: true,
+};
+
+mongoose.connect(mongoUri, connectionParams).then(() => {
+  app.listen(port, '0.0.0.0', () => {
+    console.log("Connected to MongoDB Atlas & app listening on port " + port);
   });
-});
+})
+  .catch((err) => {
+    console.error("Error connecting to MongoDB Atlas:", err);
+  });
