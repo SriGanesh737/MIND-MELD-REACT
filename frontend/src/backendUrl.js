b45e8e5c64ff8774001013db82173b7348cb5610
MV459 <saiavinashmandava45@gmail.com>
2024-05-01 22:45:03 +0530

    BackendUrl vercel

--- frontend/src/backendUrl.js
+++ frontend/src/backendUrl.js
@@ -1,2 +1,3 @@
-export const backendUrl = "http://localhost:8000";
+// export const backendUrl = "http://localhost:8000";
 // export const backendUrl = "http://3.108.54.68:8000";
+export const backendUrl = "https://mind-meld-react.onrender.com";
